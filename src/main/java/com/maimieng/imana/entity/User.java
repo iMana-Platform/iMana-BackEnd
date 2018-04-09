621ec2ced881f4400e1c46819a6b8c6a8434495c
kingcos <2821836721v@gmail.com>
2018-04-10 02:55:11 +0800

    Add Entity User

---
+++ src/main/java/com/maimieng/imana/entity/User.java
@@ -0,0 +1,88 @@
+package com.maimieng.imana.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
+
+    // ID
+    @Id
+    @GeneratedValue
+    private Integer id;
+    // 用户名
+    private String name;
+    // 密码
+    private String password;
+    // 手机号
+    private String telephone;
+    // 级别
+    private Integer level;
+    // 头像 URL
+    private String iconURL;
+    // 描述
+    private String description;
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public String getIconURL() {
+        return iconURL;
+    }
+
+    public void setIconURL(String iconURL) {
+        this.iconURL = iconURL;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
